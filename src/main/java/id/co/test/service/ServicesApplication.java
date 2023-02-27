f162b4b05918f7da7d85831a5d2ba62e426e3194
Muhammad Renardi Haris <renardi.haris@nds.co.id>
2023-02-27 16:58:17 +0700

    init project

---
+++ src/main/java/id/co/test/service/ServicesApplication.java
@@ -0,0 +1,18 @@
+package id.co.test.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "id.co.test.service*")
+@EnableAsync
+public class ServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesApplication.class, args);
+		
+		System.out.println("pre-Test");
+	}
+}
